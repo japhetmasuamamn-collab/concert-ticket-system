43eb18d248a82f92a80a527399f14928d40bc132
japhetmasuamamn-collab <japhetmasuamamn@gmail.com>
2026-07-16 03:36:03 -0700

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@
 
 // En développement local : "http://localhost:8000"
 // Si tu testes sur mobile/réseau local : remplace par l'IP de ta machine (ex: "http://192.168.1.50:8000")
-const VITE_API_URL = "https://concert-ticket-system-1.onrender.com";
+const API_BASE_URL = "https://concert-ticket-system-1.onrender.com";
 // const API_BASE_URL = "http://192.168.11.105:8000";
 
-export default VITE_API_URL;
+export default API_BASE_URL;
